5c0bf5a0ee9a1d9a73e8d7ace3dd4a01e5bfd854
Mehmet Baran Özdeniz <ozdenizm6@gmail.com>
2025-03-19 19:56:22 +0300

    Fixed CORS error

--- src/main/java/com/payment/PaymentApplication.java
+++ src/main/java/com/payment/PaymentApplication.java
@@ -10,13 +10,17 @@ import com.payment.service.impl.PaymentServiceImpl;
 import io.dropwizard.core.Application;
 import io.dropwizard.core.setup.Bootstrap;
 import io.dropwizard.core.setup.Environment;
+import jakarta.servlet.DispatcherType;
+import jakarta.servlet.FilterRegistration;
 import liquibase.Liquibase;
 import liquibase.database.jvm.JdbcConnection;
 import liquibase.resource.ClassLoaderResourceAccessor;
+import org.eclipse.jetty.servlets.CrossOriginFilter;
 import org.jdbi.v3.core.Jdbi;
 import org.jdbi.v3.sqlobject.SqlObjectPlugin;
 
 import java.sql.Connection;
+import java.util.EnumSet;
 import java.util.Objects;
 
 public class PaymentApplication extends Application<PaymentConfiguration> {
@@ -38,6 +42,18 @@ public class PaymentApplication extends Application<PaymentConfiguration> {
         Connection connection = configuration.getDatabase().build(environment.metrics(), "liquibase").getConnection();
         Liquibase liquibase = new Liquibase(liquibaseFactory.getChangelog(), new ClassLoaderResourceAccessor(), new JdbcConnection(connection));
         liquibase.update(configuration.getLiquibase().getContexts());
+
+        // Enable CORS headers
+        final FilterRegistration.Dynamic cors =
+                environment.servlets().addFilter("CORS", CrossOriginFilter.class);
+
+        // Configure CORS parameters
+        cors.setInitParameter("allowedOrigins", "*");
+        cors.setInitParameter("allowedHeaders", "X-Requested-With,Content-Type,Accept,Origin");
+        cors.setInitParameter("allowedMethods", "OPTIONS,GET,PUT,POST,DELETE,HEAD");
+
+        // Add URL mapping
+        cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
 
 
         // JDBI was enabled in the relevant database.
